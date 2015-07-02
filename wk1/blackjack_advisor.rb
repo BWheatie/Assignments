e546077bb3afbf89f25862d52bb00cd39c7307ba
Ben Wheat <BWheati@gmail.com>
2015-07-02 00:15:11 -0500

    An attempt at getting card info

--- wk1/blackjack_advisor.rb
+++ wk1/blackjack_advisor.rb
@@ -4,7 +4,7 @@ s = "stand"
 ds = "double or stand"
 p = "split"
 moves = {
-	hard = {
+	"hard" = {
 		:"5"  => {:"2" =>  h, :"3" =>  h, :"4" =>  h, :"5" =>  h, :"6" =>  h, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"6"  => {:"2" =>  h, :"3" =>  h, :"4" =>  h, :"5" =>  h, :"6" =>  h, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"7"  => {:"2" =>  h, :"3" =>  h, :"4" =>  h, :"5" =>  h, :"6" =>  h, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
@@ -23,7 +23,7 @@ moves = {
 		:"20" => {:"2" =>  h, :"3" =>  h, :"4" =>  s, :"5" =>  s, :"6" =>  s, :"7" =>  s, :"8" =>  s, :"9" =>  s, :"10" =>  s, :"A" =>  s}
 		:"21" => {:"2" =>  h, :"3" =>  h, :"4" =>  s, :"5" =>  s, :"6" =>  s, :"7" =>  s, :"8" =>  s, :"9" =>  s, :"10" =>  s, :"A" =>  s}
 		},
-	soft = {
+	"soft" = {
 		:"13" => {:"2" =>  h, :"3" =>  h, :"4" => dh, :"5" => dh, :"6" => dh, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"14" => {:"2" =>  h, :"3" =>  h, :"4" => dh, :"5" => dh, :"6" => dh, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"15" => {:"2" =>  h, :"3" =>  h, :"4" => dh, :"5" => dh, :"6" => dh, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
@@ -34,7 +34,7 @@ moves = {
 		:"20" => {:"2" =>  s, :"3" =>  s, :"4" =>  s, :"5" =>  s, :"6" =>  s, :"7" =>  s, :"8" =>  s, :"9" =>  s, :"10" =>  s, :"A" =>  s}
 		:"21" => {:"2" =>  s, :"3" =>  s, :"4" =>  s, :"5" =>  s, :"6" =>  s, :"7" =>  s, :"8" =>  s, :"9" =>  s, :"10" =>  s, :"A" =>  s}
 		},
-	pair = {
+	"pair" = {
 		:"2,2"   => {:"2" =>  p, :"3" =>  p, :"4" =>  p, :"5" =>  p, :"6" =>  p, :"7" =>  p, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"3,3"   => {:"2" =>  p, :"3" =>  p, :"4" =>  p, :"5" =>  p, :"6" =>  p, :"7" =>  p, :"8" =>  p, :"9" =>  h, :"10" =>  h, :"A" =>  h}
 		:"4,4"   => {:"2" =>  h, :"3" =>  h, :"4" =>  p, :"5" =>  p, :"6" =>  p, :"7" =>  h, :"8" =>  h, :"9" =>  h, :"10" =>  h, :"A" =>  h}
@@ -46,24 +46,29 @@ moves = {
 		:"10,10" => {:"2" =>  s, :"3" =>  s, :"4" =>  s, :"5" =>  s, :"6" =>  s, :"7" =>  s, :"8" =>  s, :"9" =>  s, :"10" =>  s, :"A" =>  s}
 		:"A,A"   => {:"2" =>  p, :"3" =>  p, :"4" =>  p, :"5" =>  p, :"6" =>  p, :"7" =>  p, :"8" =>  p, :"9" =>  p, :"10" =>  p, :"A" =>  p}
 		}
-	face = {
+	"face" = {
 		:"10" => {"j" && "Jack", "q" && "Queen", "k" && "King"}
 	} 
 }
 
+		puts "Please enter your first card:"
+			fcard = gets.chomp
+		puts "Please enter your second card:"
+			scard = gets.chomp
+		total = fcard.to_i + scard.to_i
+		puts total
+			if fcard || scard == "A" do |s|
+				s[]
+			else 	
+			end
+
+	
+# puts "Please enter the dealer's card:"
+# 	dcard = gets.chomp
 
 
-puts "Please enter your first card:"
-	fcard = gets.chomp
-puts "Please enter your second card:"
-	scard = gets.chomp
-
-puts "Please enter the dealer's card:"
-	dcard = gets.chomp
-
-
-puts "Your optimal move is to stand."
-puts "Your optimal move is to hit."
-puts "Your optimal move is to double if possible, otherwise hit."
-puts "Your optimal move is to split."
-puts "Your optimal move is to double if possible, otherwise stand."
+# puts "Your optimal move is to stand."
+# puts "Your optimal move is to hit."
+# puts "Your optimal move is to double if possible, otherwise hit."
+# puts "Your optimal move is to split."
+# puts "Your optimal move is to double if possible, otherwise stand."
